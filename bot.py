76c38e40713759c7ae31b2265b915923063b82ab
Jolly Mate <111024435+jollymate@users.noreply.github.com>
2023-04-15 01:07:49 +0300

    crush fix

--- bot.py
+++ bot.py
@@ -53,49 +53,52 @@ def friends():
 thr_st(friends, ())
 
 while True:
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and not event.from_me:
-            ch = random.choice(chance)
-            msg_id = event.message_id
-            event_msg = vk.messages.getById(message_ids=msg_id)
-            try:
-                rep_to_user = event_msg['items'][0]['reply_message']['from_id']
-                if rep_to_user == cfg.acc_id:
-                    ch = 1
-            except:
-                pass
-            msg = random.choice(msgs)
-            peer_id = event.peer_id
-            chat_id = peer_id - 2000000000
-            user_id = event.user_id
-            if event.text and chat_id < 0:
-                kf_pos = event.text.find("https://vk.me/join/")
-                if kf_pos > -1:
-                    link = event.text[kf_pos:].split(" ")[0]
-                    vk.messages.joinChatByInviteLink(link=link)
-            if ch == 1 and chat_id not in chat_ignore and user_id not in user_ignore and user_id > 0:
+    try:
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW and not event.from_me:
+                ch = random.choice(chance)
+                msg_id = event.message_id
+                event_msg = vk.messages.getById(message_ids=msg_id)
                 try:
-                    vk.messages.setActivity(
-                        type="typing",
-                        peer_id=peer_id
-                    )
+                    rep_to_user = event_msg['items'][0]['reply_message']['from_id']
+                    if rep_to_user == cfg.acc_id:
+                        ch = 1
                 except:
                     pass
-                time.sleep(cfg.send_delay)
-                try:
-                    vk.messages.send(
-                        peer_id=peer_id,
-                        random_id=random.randint(1, 999999),
-                        message=msg,
-                        reply_to=msg_id
-                    )
-                except vk_api.Captcha as e:
-                    answer = vc.solve(sid=e.sid, s=1)
-                    vk.messages.send(
-                        peer_id=peer_id,
-                        message=msg,
-                        random_id=random.randint(1, 999999),
-                        reply_to=msg_id,
-                        captcha_key=answer,
-                        captcha_sid=e.sid
-                    )
+                msg = random.choice(msgs)
+                peer_id = event.peer_id
+                chat_id = peer_id - 2000000000
+                user_id = event.user_id
+                if event.text and chat_id < 0:
+                    kf_pos = event.text.find("https://vk.me/join/")
+                    if kf_pos > -1:
+                        link = event.text[kf_pos:].split(" ")[0]
+                        vk.messages.joinChatByInviteLink(link=link)
+                if ch == 1 and chat_id not in chat_ignore and user_id not in user_ignore and user_id > 0:
+                    try:
+                        vk.messages.setActivity(
+                            type="typing",
+                            peer_id=peer_id
+                        )
+                    except:
+                        pass
+                    time.sleep(cfg.send_delay)
+                    try:
+                        vk.messages.send(
+                            peer_id=peer_id,
+                            random_id=random.randint(1, 999999),
+                            message=msg,
+                            reply_to=msg_id
+                        )
+                    except vk_api.Captcha as e:
+                        answer = vc.solve(sid=e.sid, s=1)
+                        vk.messages.send(
+                            peer_id=peer_id,
+                            message=msg,
+                            random_id=random.randint(1, 999999),
+                            reply_to=msg_id,
+                            captcha_key=answer,
+                            captcha_sid=e.sid
+                        )
+    except:
+        pass
